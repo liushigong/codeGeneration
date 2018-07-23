633ba057f4992e6ca2ba5287c9a0e916e1ee5c5e
liushigong <15101101033@163.com>
2018-07-23 23:42:32 +0800

    demo

---
+++ src/main/java/com/page/demo/controller/PageDemoController.java
@@ -0,0 +1,35 @@
+package com.page.demo.controller;
+
+import com.page.demo.entity.City;
+import com.page.demo.service.CityService;
+import com.page.utils.pagination.Page;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+@Controller
+public class PageDemoController {
+
+    @Autowired
+    CityService cityService;
+
+    @RequestMapping("/demo")
+    public String pageDemo(Page<City> page,City city, HttpServletRequest request) {
+        page.setObj(city);
+        /*Map<String,Object> params = new HashMap<>();
+        if(city.getCode() != null){
+            params.put("code","%"+city.getCode()+"%");
+        }
+        page.setParams(params);*/
+        List<City> cityList = cityService.findPage(page);
+        request.setAttribute("cityList", cityList);
+        request.setAttribute("com/page", page);
+        request.setAttribute("city", city);
+        return "/index";
+    }
+
+
+}
